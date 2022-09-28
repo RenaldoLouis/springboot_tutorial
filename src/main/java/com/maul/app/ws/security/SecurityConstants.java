6f5ebd2a8a40c17279cf9549cf1afc9b0c0ee7d1
Renaldo Louis <renaldo.louis@commsult.id>
2022-09-28 11:07:43 +0700

    pindahin token secret dari class java ke env property and theres a simple way how to acces a component on another class using getBEAN from springApplicationContext with param name of component we want to use

--- src/main/java/com/maul/app/ws/security/SecurityConstants.java
+++ src/main/java/com/maul/app/ws/security/SecurityConstants.java
@@ -1,9 +1,15 @@
 package com.maul.app.ws.security;
 
+import com.maul.app.ws.SpringApplicationContext;
+
 public class SecurityConstants {
 	public static final long EXPIRATION_TIME = 864000000; // 10 days
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGN_UP_URL = "/maul/signUp";
-	public static final String TOKEN_SECRET = "2dae84f846e4f4b158a8d26681707f4338495bc7ab68151d7f7679cc5e56202dd3da0d356da007a7c28cb0b780418f4f3246769972d6feaa8f610c7d1e7ecf6a";
+
+	public static String getTokenSecret() {
+		AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+		return appProperties.getTokenSecret();
+	}
 }
